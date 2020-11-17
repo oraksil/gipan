a2082120a683227420d12743d8977420778bc94a
gamz <gamzabaw@gmail.com>
2020-11-18 06:18:30 +0900

    Update docker entiry

--- docker-entry.sh
+++ docker-entry.sh
@@ -4,9 +4,8 @@ LD_LIBRARY_PATH=$APP_HOME/mame \
   ./gipan \
   --imageframe-output $IPC_IMAGE_FRAMES \
   --soundframe-output $IPC_SOUND_FRAMES \
-  --key-input $IPC_KEY_INPUTS \
-  --resolution 640x480 \
-  --fps 25 \
-  --keyframe-interval 150 \
-  --idle-time-to-enc-sleep 300 \
+  --cmd-input $IPC_CMD_INPUTS \
+  --resolution $RESOLUTION \
+  --fps $FPS \
+  --keyframe-interval $KEYFRAME_INTERVAL \
   --game $GAME
