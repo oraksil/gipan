12186e70f1bd56eee350826fe6f3e2f6783f7cf6
gamz <gamzabaw@gmail.com>
2020-12-03 21:21:03 +0900

    Refactoring

--- src/roms.rs
+++ src/roms.rs
@@ -20,17 +20,7 @@ impl GcpRomManager {
             base_roms_path: String::from(roms_path)
         }
     }
-}
 
-fn download_url_to_path(url: &str, path: &Path) {
-    let response = reqwest::blocking::get(url).unwrap();
-    let content =  response.bytes().unwrap();
-
-    let mut file = File::create(path).unwrap();
-    file.write_all(&content).unwrap();
-}
-
-impl GcpRomManager {
     fn download_objects(&self, objects: &Vec<Object>) {
         let join_handles: Vec<std::thread::JoinHandle<_>> = objects.into_iter()
             .map(|o| { 
@@ -39,7 +29,7 @@ impl GcpRomManager {
                 let url = o.download_url(120).unwrap();
                 let filename = Path::new(&o.name).file_name().unwrap();
                 let path = Path::new(&self.base_roms_path).join(filename);
-                thread::spawn(move || download_url_to_path(&url, &path))
+                thread::spawn(move || gcp_download_url_to_path(&url, &path))
             })
             .collect();
 
@@ -47,18 +37,32 @@ impl GcpRomManager {
             let _ = h.join();
         }
     }
+
+    fn list_rom_objects(&self, emu_type: &str, system_name: &str) -> Vec<Object> {
+        let prefix = format!("{}/{}/", emu_type, system_name);
+        Object::list_prefix("oraksil-games", &prefix).unwrap()
+            .into_iter()
+            .filter(|r| r.name != prefix)
+            .collect()
+    }
+}
+
+fn gcp_download_url_to_path(url: &str, path: &Path) {
+    let response = reqwest::blocking::get(url).unwrap();
+    let content =  response.bytes().unwrap();
+
+    let mut file = File::create(path).unwrap();
+    file.write_all(&content).unwrap();
 }
 
 impl RomManager for GcpRomManager {
     fn pull_roms(&self, emu_type: &str, system_name: &str) -> Result<(), String> {
-        let prefix = format!("{}/{}/", emu_type, system_name);
-        let rom_objects = Object::list_prefix("oraksil-games", &prefix).unwrap();
-        let filtered = rom_objects.into_iter()
-            .filter(|r| r.name != prefix)
-            .collect();
+        let rom_objects = self.list_rom_objects(&emu_type, &system_name);
             
-        self.download_objects(&filtered);
+        self.download_objects(&rom_objects);
+
         println!("download done..");
+
         Ok(())
     }
 }
